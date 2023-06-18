801de6ae4514e37ff8d4b380e6173160c33fab12
Hoang Do <tuanhoang2do2311@gmail.com>
2023-06-18 04:48:34 +0000

    modify project structure

--- data/eliza_service_test_script.py
+++
@@ -1,7 +0,0 @@
-from keywordrecognition.models import ElizaBot
-from keywordrecognition.services import ElizaService
-
-elizabot = ElizaBot.objects.get(id=7)
-eliza_service = ElizaService(bot=elizabot)
-response = eliza_service.response("bạn là búp măng lon")
-print(response)
