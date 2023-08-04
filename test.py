c5bdb8de52737d88aa97ff7f3852eec8e5ee2774
Hoang Do <tuanhoang2do2311@gmail.com>
2023-08-04 18:29:39 +0000

    deactive test file

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from nlp.models import NLPBot
-from nlp.services import NLPService
+# from nlp.models import NLPBot
+# from nlp.services import NLPService
 
-bot = NLPBot.objects.get(id=8)
-nlp_service = NLPService(bot=bot, context={})
-nlp_service.train_model()
-print(nlp_service.generate_response("đúng rồi nha"))
+# bot = NLPBot.objects.get(id=8)
+# nlp_service = NLPService(bot=bot, context={})
+# nlp_service.train_model()
+# print(nlp_service.generate_response("đúng rồi nha"))
